979f9e6f9be73607f9e73c06811a03c9a02d595e
Babenko Liudmyla <savelieva0509@gmail.com>
2023-12-07 18:13:16 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from 'App';
-import { Provider } from 'react-redux';
+//import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 // import { persistor, store } from '../src/redux/store';
