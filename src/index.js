6deb0603c11f012e79bf3f3fcfbbc003b8fb5ba0
Babenko Liudmyla <savelieva0509@gmail.com>
2023-12-10 19:38:10 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from 'App';
-//import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { store, persistor } from './redux/store';
+import App from './App';
 import './index.css';
-// import { persistor, store } from '../src/redux/store';
-// import { PersistGate } from 'redux-persist/integration/react';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    {/* <Provider> */}
-      {/* <PersistGate loading={null} persistor={persistor}> */}
-        <BrowserRouter basename="/car-rental-app">
-          <App />
-        </BrowserRouter>
-      {/* </PersistGate> */}
-    {/* </Provider> */}
-  </React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <Router>
+        <App />
+      </Router>
+    </PersistGate>
+  </Provider>
 );
