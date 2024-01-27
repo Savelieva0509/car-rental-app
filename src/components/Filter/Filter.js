f6227adaf6103cb446338554a30c1426b73139c4
Babenko Liudmyla <savelieva0509@gmail.com>
2024-01-27 16:14:24 +0200

    Update Filter.js

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Select from 'react-select';
 import Button from 'components/Button/Button';
 import css from './Filter.module.css';
