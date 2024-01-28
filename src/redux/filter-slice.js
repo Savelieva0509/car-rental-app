4bdc1d50da5ae487e2d7d5d52d87693d5d158612
Babenko Liudmyla <savelieva0509@gmail.com>
2024-01-28 17:51:02 +0200

    add Mileage state

--- src/redux/filter-slice.js
+++ src/redux/filter-slice.js
@@ -5,6 +5,10 @@ const filterSlice = createSlice({
   initialState: {
     selectedMake: null,
     selectedPrice: null,
+    selectedMileage: {
+      min: null,
+      max: null,
+    },
   },
   reducers: {
     setFilterMake: (state, action) => {
@@ -13,19 +17,36 @@ const filterSlice = createSlice({
     setFilterPrice: (state, action) => {
       state.selectedPrice = action.payload;
     },
+    setFilterMinMileage: (state, action) => {
+      state.selectedMileage.min = action.payload;
+    },
+    setFilterMaxMileage: (state, action) => {
+      state.selectedMileage.max = action.payload;
+    },
   },
 });
 
-export const { setFilterMake, setFilterPrice } = filterSlice.actions;
+export const {
+  setFilterMake,
+  setFilterPrice,
+  setFilterMinMileage,
+  setFilterMaxMileage,
+} = filterSlice.actions;
 
 export const selectSelectedMake = state => {
-  
   return state.filterCars.selectedMake;
 };
 
 export const selectSelectedPrice = state => {
-  
   return state.filterCars.selectedPrice;
+};
+
+export const selectMinMileage = state => {
+  return state.filterCars.selectedMileage.min;
+};
+
+export const selectMaxMileage = state => {
+  return state.filterCars.selectedMileage.max;
 };
 
 export const filterReducer = filterSlice.reducer;
