398b52b1b35c42ca17e146c397e0d363f6ad2df9
Babenko Liudmyla <savelieva0509@gmail.com>
2024-06-10 13:07:03 +0300

    Update Api.js

--- src/Api.js
+++ src/Api.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-axios.defaults.baseURL = 'https://6571f80ad61ba6fcc0141958.mockapi.io';
+axios.defaults.baseURL =
+  'https://6666cf0ba2f8516ff7a50825.mockapi.io';
 
 export const getTotalCars = async () => {
   try {
